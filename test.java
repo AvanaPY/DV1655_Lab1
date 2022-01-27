8aae7175a5a808058602841aaf332b670237683d
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2022-01-27 11:05:18 +0100

    uhh samuel stuff

--- test.java
+++ test.java
@@ -1,17 +1,18 @@
 class Main {
    public static void main(String[] args ){
       while(a == 0){
-         System.out.println(notyeet)
+         System.out.println(notyeet);
       }
-      return null;
    }
 }
 
 class Abcyeee {
    int[] a;
+   int b;
    public void yeet(float name){
       while(a == b){
-         System.out.println(yeet)
-      };
-   }
-}
+         b=2;
+         System.out.println(yeet);
+      }
+   
+   }}
