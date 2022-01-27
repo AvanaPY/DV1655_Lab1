3aca578aba6e731ff9d15fea595e079f3318783e
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2022-01-27 11:17:53 +0100

    Minor updates

--- test.java
+++ test.java
@@ -6,13 +6,16 @@ class Main {
    }
 }
 
-class Abcyeee {
+class Abcyeee extends Abc{
    int[] a;
    int b;
    public void yeet(float name){
       while(a == b){
          b=2;
          System.out.println(yeet);
-      }
-   
-   }}
+      }   
+   }
+   public void again(float more_name){
+      System.out.println(yeettoo);
+   }
+}
