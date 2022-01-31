e4525a6522550c7a99a624410ec23a80b948bb76
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2022-01-31 16:13:43 +0100

    More test stuff

---
+++ examples/multi_method.java
@@ -0,0 +1,13 @@
+class Main {
+   public static void main(String[] args ){
+       a = b;
+   }
+}
+
+class Main1{
+    public void method1(int a){}
+    public void method2(int a, int b){}
+    public void method3(int b){}
+    public void method4(int c, boolean b){}
+    public void method5(int[] c, boolean a){}
+}
