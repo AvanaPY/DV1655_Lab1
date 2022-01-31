4f55cf9980b2edf342406a6ef18f1aa15052439c
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2022-01-31 16:24:02 +0100

    yeet

--- examples/nested_while.java
+++ examples/nested_while.java
@@ -1,12 +1,11 @@
 class Main {
    public static void main(String[] args ){
       while(a == 0){
-         while(b == 0){
+         while(b == 0)
             if(a == b)
                b = a;
             else
                a = b;
-         }
       }
    }
 }
