333cc082bf66e4cfd7562c14b911710d8224c3d4
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2022-02-02 17:00:58 +0100

    next level java holy shit

--- examples/works.java
+++ examples/works.java
@@ -1,17 +1,21 @@
 class Main {
    public static void main(String[] args){
       System.out.println(5);
+      BigBrain.run();
    }
 }
 
 class BigBrain{
    int var1;
-   public void run(int a, int b, int[] c){
-
-   }
-   public void run2()
+   public void run()
    {
       int a;
-      a = 5 + -2;
+      int b;
+      int[] c;
+      c = 2;
+      this.run2(a, b, c);
+   }
+   public void run2(int a, int b, int[] c)
+   {
    }
 }
